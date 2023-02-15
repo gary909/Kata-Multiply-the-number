119ae9ee1965a5fd17fb5fb8283c92a3c5b5d55c
Gary White <garywhite61@hotmail.com>
2023-02-15 14:06:19 +0100

    code working

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
+// function multiply(number){
+//     //returns number to be sqrd by;
+//     if (number < 0){
+//         let numCount = number.toString().length -1;
+//     } else {
+//         numCount = number.toString().length;
+//     }
+//     return number * 5 ** numCount;
+// }
+
 function multiply(number){
-    //your code here
+    return number * 5 ** String(Math.abs(number)).length;
 }
 
 console.log(multiply(10)); // 250
 console.log(multiply(5)); // 25
 console.log(multiply(200)); // 250000
-console.log(multiply(0)); // 0
+console.log(multiply(0)); // 
 console.log(multiply(-2)); // -10
