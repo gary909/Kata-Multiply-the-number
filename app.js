2526a566c044ef241dd8a88fe5bad7a3f5081df0
Gary White <garywhite61@hotmail.com>
2023-02-15 13:11:30 +0100

    code

---
+++ app.js
@@ -0,0 +1,9 @@
+function multiply(number){
+    //your code here
+}
+
+console.log(multiply(10)); // 250
+console.log(multiply(5)); // 25
+console.log(multiply(200)); // 250000
+console.log(multiply(0)); // 0
+console.log(multiply(-2)); // -10
